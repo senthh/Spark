6080258c4faeb3d774a580563cd98e2026111123
senthh <senthh@gmail.com>
2018-12-17 02:22:04 +0530

    Add files via upload

--- SparkOnHbase/src/main/java/SparkOnHbase.java
+++ SparkOnHbase/src/main/java/SparkOnHbase.java
@@ -33,7 +33,9 @@ import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapred.TableOutputFormat;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
@@ -42,7 +44,7 @@ import org.apache.hadoop.hbase.client.ConnectionFactory;
 import scala.Tuple2;
 
 public class SparkOnHbase {
-	private static Object String;
+	//private static Object String;
 
 	public static void main(String[] args) { 
 		  
@@ -58,36 +60,77 @@ public class SparkOnHbase {
 				  .getOrCreate();
 		  Configuration hconf = HBaseConfiguration.create();
 		  //hconf.set("hbase.master", "localhost:59139");
-		  hconf.set(TableInputFormat.INPUT_TABLE, "test3");
+		  hconf.set(TableInputFormat.INPUT_TABLE, "RiskSense1");
 		  //hconf.set("hbase-site.xml\"");
 		  hconf.addResource("hbase-site.xml");
 	     // String tableName = "HBase.CounData1_Raw_Min1";
-		  TableName tName= TableName.valueOf("test3");
+		  TableName tName= TableName.valueOf("RiskSense1");
 		  JavaPairRDD<ImmutableBytesWritable, Result> source;
 		  /*= javaSparkContext.newAPIHadoopRDD(hconf, TableInputFormat.class,
                   ImmutableBytesWritable.class, Result.class);*/
 		  HTableDescriptor htable = new HTableDescriptor(tName);
 		  Table table;
+		  //Put put;
 	      try
 	      {
 	    	  //	HBaseAdmin hadmin = new HBaseAdmin(hconf);
-	    	  	 
+	    	  	Dataset<Row> df = sparkSession.read().parquet("/Users/senthilkumar/Documents/Senthil/RiskSense/asset/RiskSense_data.parquet").toDF();
+			df.printSchema();
+			//JavaPairRDD<String,String> assetsContentsRDD = df.javaRDD();
+			
+			/*df.map(new Function<Tuple2<String, String>, String>() {
+				@Override
+				public String call(Tuple2<String, String> assetContent) throws Exception {
+					return assetContent._2();
+       
+				}
+			});*/
+			//df.write().options(Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "5"))
+	    	  	
 	    	  	Connection conn = ConnectionFactory.createConnection(hconf);
 	    	  	Admin hadmin = conn.getAdmin();
+	    	  	
 	    	  	if(!hadmin.isTableAvailable(tName))
 	    	  	{	
 	    	  		System.out.println("Table "+tName+" doesn't exists...");
 	    	  		System.out.println("Creating Table "+tName+"...");
 	    	  		 
-	      	  	htable.addFamily( new HColumnDescriptor("Id"));
-	      	  	htable.addFamily( new HColumnDescriptor("Name"));
+	      	  	htable.addFamily( new HColumnDescriptor("Network"));
+	      	  	//htable.addFamily( new HColumnDescriptor("fqdn"));
 	    	  		hadmin.createTable(htable);
-	    	  		Put p = new Put(Bytes.toBytes("row1"));
-	    	  		//p.addColumn(family, qualifier, value)
-	    	  		p.addColumn(Bytes.toBytes("Id"), Bytes.toBytes("col1"),Bytes.toBytes("1"));
-	    	  	    p.addColumn(Bytes.toBytes("Name"),Bytes.toBytes("col2"),Bytes.toBytes("SenthilKumar"));
-	    	  	    table = conn.getTable(tName);
-	    	  	    table.put(p);
+	    	  		source = javaSparkContext.newAPIHadoopRDD(hconf, TableInputFormat.class,
+	    	                  ImmutableBytesWritable.class, Result.class);
+	    	  		JavaPairRDD<ImmutableBytesWritable, Put> hbasePuts = df.javaRDD().mapToPair(
+	  				      new PairFunction<Row, ImmutableBytesWritable, Put>() {
+	  				    	  	//@Override
+	  				    	  	public Tuple2<ImmutableBytesWritable, Put> call(Row row) throws Exception {
+	  				         
+	  				    	  		Put put = new Put(Bytes.toBytes(row.getString(0)));
+	  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("dns"), Bytes.toBytes(row.getString(1)));
+	  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("fqdn"), Bytes.toBytes(row.getString(2)));
+	  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("ip"), Bytes.toBytes(row.getString(3)));
+	  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("port"), Bytes.toBytes(row.getString(4)));
+	  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("qid"), Bytes.toBytes(row.getString(5)));
+	  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("severity_level"), Bytes.toBytes(row.getString(6)));
+	  				    	  		return new Tuple2<ImmutableBytesWritable, Put>(new ImmutableBytesWritable(), put);     
+	  				    }
+	  			});
+	    	  		
+	    	  		if(source.count()>0)
+	    	  		{
+	    	  			Job newAPIJobConfiguration1 = Job.getInstance(hconf);	
+		    	  		newAPIJobConfiguration1.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "RiskSense1");
+		    	  		newAPIJobConfiguration1.setOutputFormatClass(org.apache.hadoop.hbase.mapreduce.TableOutputFormat.class);
+	    	  			System.out.println("Inserting into HBase Table...");
+		    	  		System.out.println("Data: "+hbasePuts.collect());
+			  		hbasePuts.saveAsNewAPIHadoopDataset(newAPIJobConfiguration1.getConfiguration());
+	    	  		}
+	    	  		else
+	    	  		{
+	    	  			System.out.println("Parquet Data is empty ...");
+	    	  		}
+	    	  	   // table = conn.getTable(tName);
+	    	  	    //table.put(put);
 	    	  	}
 	    	  	else
 	    	  	{
@@ -95,18 +138,54 @@ public class SparkOnHbase {
 	    	  		Scan scan = new Scan();
 	    	  		source = javaSparkContext.newAPIHadoopRDD(hconf, TableInputFormat.class,
 	    	                  ImmutableBytesWritable.class, Result.class);
-	    	  		scan.addColumn(Bytes.toBytes("Id"), Bytes.toBytes("col1"));
-	    	  		scan.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("col2"));
+	    	  		
+	    	  		JavaPairRDD<ImmutableBytesWritable, Put> hbasePuts = df.javaRDD().mapToPair(
+		  				      new PairFunction<Row, ImmutableBytesWritable, Put>() {
+		  				    	  	//@Override
+		  				    	  	public Tuple2<ImmutableBytesWritable, Put> call(Row row) throws Exception {
+		  				         
+		  				    	  		Put put = new Put(Bytes.toBytes(row.getString(0)));
+		  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("dns"), Bytes.toBytes(row.getString(1)));
+		  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("fqdn"), Bytes.toBytes(row.getString(2)));
+		  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("ip"), Bytes.toBytes(row.getString(3)));
+		  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("port"), Bytes.toBytes(row.getString(4)));
+		  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("qid"), Bytes.toBytes(row.getString(5)));
+		  				    	  		put.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("severity_level"), Bytes.toBytes(row.getString(6)));
+		  				    	  		return new Tuple2<ImmutableBytesWritable, Put>(new ImmutableBytesWritable(), put);     
+		  				    }
+		  			});
+	    	  		Job newAPIJobConfiguration1 = Job.getInstance(hconf);	
+	    	  		newAPIJobConfiguration1.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "RiskSense1");
+	    	  		newAPIJobConfiguration1.setOutputFormatClass(org.apache.hadoop.hbase.mapreduce.TableOutputFormat.class);
+	    	  		if(source.count()>0)
+	    	  		{
+	    	  			System.out.println("Inserting into HBase Table...");
+		    	  		System.out.println("Data: "+hbasePuts.collect());
+			  		hbasePuts.saveAsNewAPIHadoopDataset(newAPIJobConfiguration1.getConfiguration());
+	    	  		}
+	    	  		else
+	    	  		{
+	    	  			System.out.println("Parquet Data is empty ...");
+	    	  		}
+	    	  		
+		  		//hbasePuts.sav
+		  			
 	    	  		ResultScanner scanner = conn.getTable(tName).getScanner(scan);
 	    	  		//System.out.println(hadmin.listTableNames());
 	    	  		System.out.println("Checking record count of table "+tName+": "+source.count());
 	    	  		//System.out.println(source.collect());;
+	    	  		
+	    	  		//Input Data is not matching properly so parquet table is created as empty always...
+	    	  		//So not able to perform rest of the questions..
 	    	  		Result result;
 	    	  		for (result = scanner.next(); result != null; result = scanner.next())
-
-	    	  	    System.out.println("Found row : " + (result.getValue(Bytes.toBytes("Name"), Bytes.toBytes("col2"))).toString());
+	    	  		{
+	    	  			System.out.println("Found row : " + (result.getValue(Bytes.toBytes("Network"), Bytes.toBytes("dns"))).toString());
 	    	  	      //closing the scanner
-	    	  		System.out.println("Found row : " + result);
+	    	  			System.out.println("Found row : " + result.getRow().toString());
+	    	  		
+	    	  		}
+	    	  		//result.getRow().toString().
 	    	  	      scanner.close();
 	    	  	
 	    	  		
